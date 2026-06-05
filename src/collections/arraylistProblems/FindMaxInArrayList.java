a3292ce1ef8359d7306100587c8b769ef5bc1e21
vinnacodes <vinnarasu2000@gmail.com>
2026-06-05 11:59:25 +0530

    max value code using for loop

--- src/collections/arraylistProblems/FindMaxInArrayList.java
+++ src/collections/arraylistProblems/FindMaxInArrayList.java
@@ -1,8 +1,37 @@
 package collections.arraylistProblems;
 
+import java.util.ArrayList;
 public class FindMaxInArrayList {
 
     public static void main(String[] args) {
+
+
+       ArrayList<Integer> li = new ArrayList<>();
+
+
+       li.add(4);
+       li.add(5);
+       li.add(45);
+       li.add(3);
+       li.add(-3);
+
+       int max =li.get(0);
+
+
+
+       for (int i =0;i<li.size();i++){
+
+
+           if (li.get(i)>max){
+
+
+               max =li.get(i);
+           }
+
+       }
+
+
+        System.out.println(max);
 
     }
 
