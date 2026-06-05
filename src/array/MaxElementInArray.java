15ca10aceb3472a972fc739f2c74a960bfb0b1bc
vinnacodes <vinnarasu2000@gmail.com>
2026-06-05 10:11:38 +0530

    find the maxelement in array problem

--- src/array/MaxElementInArray.java
+++ src/array/MaxElementInArray.java
@@ -5,5 +5,21 @@ public class MaxElementInArray {
 
     public static void main(String[] args) {
 
+
+
+        maxElement();
+
     }
+
+  static  void maxElement(){
+
+
+
+        // returning the maxELement
+    }
+
+
+
+
+
 }
