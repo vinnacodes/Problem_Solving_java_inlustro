d24ac9b168c1a02c0f1dfdc3696019a3719c7c21
vinnacodes <vinnarasu2000@gmail.com>
2026-06-05 14:14:50 +0530

    doubly linked list implementation

---
+++ src/collections/linkedlist/DoublyLinkedListTes.java
@@ -0,0 +1,66 @@
+package collections.linkedlist;
+
+public class DoublyLinkedListTes {
+
+    public static void main(String[] args) {
+
+      DoublyLinkedList d1 = new DoublyLinkedList();
+
+
+
+
+        d1.InsertAtBeginning(1);
+
+        System.out.println(d1.head);
+
+
+
+    }
+
+
+
+}
+class DoublyLinkedList{
+
+    Node head ;
+
+
+
+
+
+    class Node{
+
+        Node prev ;
+
+        int data;
+
+        Node next;
+
+
+
+
+        Node(int val){
+
+            this.data =val;
+
+
+        }
+
+    }
+
+
+
+
+
+    void InsertAtBeginning(int value){
+
+       Node newnode= new Node(value);
+
+       head =newnode;
+
+
+
+    }
+
+}
+
