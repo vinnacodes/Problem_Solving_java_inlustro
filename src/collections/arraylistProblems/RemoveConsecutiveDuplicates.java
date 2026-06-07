730df4df793864d66d2a76291eeaf19c8e17fddc
vinnacodes <vinnarasu2000@gmail.com>
2026-06-07 09:02:06 +0530

    Removing consecutive duplicate elements in the ArrayList

---
+++ src/collections/arraylistProblems/RemoveConsecutiveDuplicates.java
@@ -0,0 +1,37 @@
+package collections.arraylistProblems;
+
+import java.util.ArrayList;
+
+//Remove Consecutive Duplicates
+//Given an ArrayList of integers, remove all consecutive duplicate values while keeping the order of first
+//occurrences. Non-consecutive duplicates are kept.
+//Input:
+//        [1, 1, 2, 2, 2, 3, 1, 1, 4]
+//Output:
+//        [1, 2, 3, 1, 4]
+
+
+
+
+public class RemoveConsecutiveDuplicates {
+
+
+
+    public static void main(String[] ar){
+
+        removeConsecutiveDuplicates();
+
+        //call the method from the main method
+
+
+    }
+
+
+    public  static ArrayList<Integer> removeConsecutiveDuplicates(
+            ArrayList<Integer> list){
+
+
+        // this is where the code to be written and the method signature is already given in the pdf
+    }
+
+}
